cfb1531eb6dde04118d56677cfeee4a61159b2d6
adyzbyte <75579903+adyzbyte@users.noreply.github.com>
2022-03-17 13:34:36 +0100

    Add files via upload

---
+++ dierentuin.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+	<title> Dierentuin </title>
+	<style type=text/css>
+	body   
+{  
+height: 125vh;  
+margin-top: 80px;  
+padding: 30px;  
+background-size: cover;  
+font-family: sans-serif;  
+}  
+header {  
+background-color: orange;  
+position: fixed;  
+left: 0;  
+right: 0;  
+top: 5px;  
+height: 30px;  
+display: flex;  
+align-items: center;  
+box-shadow: 0 0 25px 0 black;  
+}  
+header * {  
+display: inline;  
+}  
+header li {  
+margin: 20px;  
+}  
+header li a {  
+color: blue;  
+text-decoration: none;  
+}  
+html {
+            height: 100%;
+        }
+        body
+
+            {background-image: linear-gradient(orange, white); 
+            }
+</style>
+<div class="bg"></div>
+</head>
+<body>
+<header>
+<nav>
+<ul>  
+<li>  
+<a href="#"> Home </a>  
+</li>  
+<li>  
+<a href="#"> Over Ons </a>  
+</li>  
+<li>  
+<a href="#"> Contact </a>  
+</li>  
+ 
+</ul>  
+</header>
+</nav>
+	<form method="POST">
+
+    <h1>Dierentuin Roosendaal</h1>
+	<h2>Beleef een avontuur in de wildernis!</h2>
+
+</body>
+
+</html>
